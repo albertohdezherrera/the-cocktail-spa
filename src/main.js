314f476ab1dbedc1b26a8f9ee00dbd98c21def84
Alberto Hernández Herrera <alberto.hernandezherrera@gecolabor.com>
2021-06-24 12:24:59 +0200

    Created prototype axios and get 10 beers from api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
+import axios from 'axios';
+
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App).use(router);
+
+app.config.globalProperties.$axios = axios;
+app.mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    {{ status }}
+    {{ beersList.length }}
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -13,6 +15,22 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      status: 'LOADING',
+      beersList: undefined,
+    };
+  },
+  async created() {
+    const { $axios } = this;
+    try {
+      const result = await $axios.get('https://api.punkapi.com/v2/beers');
+      this.beersList = result.data.slice(0, 10);
+      this.status = 'SUCCESS';
+    } catch (e) {
+      this.status = 'ERROR';
+    }
   },
 };
 </script>
